26e15d5d28a932526bfec1576b22c6bd6e0e09cb
Targex25 <luki123cz@gmail.com>
2026-03-08 13:39:11 +0100

    Added support for multiple images + cleaned up the code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple tool for generating arrays with RGB data from an image. Made for [Astro p
 
 ## How to use
 
-### Python instalation
+### Instalation
 
 1. Install [Python](https://www.python.org/downloads/)
 2. Install [PIL library](https://pillow.readthedocs.io/en/stable/installation/basic-installation.html)
@@ -12,7 +12,6 @@ Simple tool for generating arrays with RGB data from an image. Made for [Astro p
 ### Program usage
 
 1. Download main.py
-2. Place your image **in the same folder as main.py**
+2. Place your images **in the same folder as main.py**
 3. Run main.py
-4. Type-in file name **and extension**
-5. Copy results wherever you want!
+4. Copy results wherever you want!
--- main.py
+++ main.py
@@ -2,51 +2,56 @@ from PIL import Image
 import os
 #Define Variables
 key = "abcdefghijklmnopqrstuvwxyz" #Key for naming variables
-ConvertedList = []
-Values = []
 VarKey = {}
-FinalOutput = []
-vars = ""
 #Inputs
+"""
 file = str(input("Please type-in your image name with extension (example: flower.png): "))
 
 while not os.path.exists(file):
     print("Selected File does not exist!")
     file = str(input("Please type-in your image name with extension (example: flower.png): "))
 #Image stuff
-im = Image.open(file).convert("RGB")
-pix = im.load()
-width,height = im.size
+"""
+for file in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+    ConvertedList = []
+    Values = []
+    FinalOutput = []
+    vars = ""
+    filename = os.fsdecode(file)
+    if filename.endswith(".png"):
+        im = Image.open(file).convert("RGB")
+        pic = im.load()
+        assert pic is not None
+        width,height = im.size
 
-#Add all RGB Values to a list
-for y in range(width):
-    for i in range(height):
-        ConvertedList.append(pix[i,y])
-        
-#Add each value only once   
-for i in ConvertedList:
-    if not i in Values:
-        Values.append(i)
+        #Add all RGB Values to a list
+        for y in range(width):
+            for i in range(height):
+                ConvertedList.append(pic[i,y])
+                
+        #Add each value only once   
+        for i in ConvertedList:
+            if not i in Values:
+                Values.append(i)
 
-#Assign values to variables
-x = 0
-for i in Values:
-    vars = vars + key[x] + " = " + str(i) + "\n"
-    #key
-    VarKey[str(i)] = str(key[x])
-    
-    x += 1
+        #Assign values to variables
+        x = 0
+        for i in Values:
+            vars = vars + key[x] + " = " + str(i) + "\n"
+            #key
+            if not str(i) in VarKey:
+                VarKey[str(i)] = str(key[x])
+            
+            x += 1
 
-#Make final list
-x = 0
-for i in ConvertedList:
-    FinalOutput.append(VarKey.get(str(i)))
-    
-#Print everything
-print("")
-print("Generated succesfully!" + "\n")
-print("#Colors")
-print(vars)
-print("#Picture")
-print("["+(', ').join(FinalOutput)+"]")
-
+        #Make final list
+        x = 0
+        for i in ConvertedList:
+            FinalOutput.append(VarKey.get(str(i)))
+        #print(VarKey)
+        #Print everything
+        #print(str(filename))
+        print("")
+        print(f"[{(', ').join(FinalOutput)}]")
+for i in VarKey:
+    print(f"{VarKey[i]} = {i}")
